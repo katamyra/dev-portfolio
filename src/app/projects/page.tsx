445ae6e75bff778ce021d115cb32848655b17017
Krish Katariya <krish.katariya@hotmail.com>
2024-02-04 02:22:23 -0500

    Working on projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,14 @@
 "use client"
 import ProjectsCard from "@/components/ui/ProjectsCard"
-export default function Projects() {
 
+export default function Projects() {
     return (
-        <ProjectsCard></ProjectsCard>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '1rem' }}>
+            <ProjectsCard></ProjectsCard>
+            <ProjectsCard></ProjectsCard>
+            <ProjectsCard></ProjectsCard>
+            <ProjectsCard></ProjectsCard>
+            {/* Add more ProjectsCard components as needed */}
+        </div>
     )
 }
