fa928d6ea45d1527471432995718a6e4ef52baf3
Krish Katariya <krish.katariya@hotmail.com>
2024-02-04 12:40:44 -0500

    Testing a projects section using contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,20 @@ import { CarouselItem } from '@/components/ui/carousel';
 import ParticleBackground from '@/components/ui/ParticleBackground';
 export default function Contact() {
     return (
-      <div className='h-full'>
-         <ParticleBackground></ParticleBackground>
+      <div>
+        <div className="flex">
+          <div className="mockup-window border bg-base-500 w-1/2">
+            <div className="flex justify-center px-4 py-16 bg-base-600">Hello!</div>
+          </div>
+          <div className="w-1/2">
+            <h1 className="text-4xl font-bold">Right Half Text</h1>
+            <p className="text-lg">Some text content here...</p>
+          </div>
+        </div>
       </div>
+        
      
+ 
           
     )
 }
