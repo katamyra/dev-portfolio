f2edb950e61327eb93bba28042c48fa8f917e820
Krish Katariya <krish.katariya@hotmail.com>
2024-02-04 02:28:05 -0500

    Removed carousel

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import ContactCarousel from '@/components/ui/ContactCarousel';
 import AnimatedTextWord from '@/components/ui/AnimatedTextWord';
 import { CarouselItem } from '@/components/ui/carousel';
 import ParticleBackground from '@/components/ui/ParticleBackground';
