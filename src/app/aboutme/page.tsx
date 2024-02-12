fbcb3f00dfcdd385fc07a8ca9ccce726c5de547d
Krish Katariya <krish.katariya@hotmail.com>
2024-02-12 01:52:52 -0500

    Modified about me for mobile

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -12,7 +12,7 @@ export default function aboutme() {
             <div className='text-center'>
                 <h1 className="text-2xl font-bold"> About Me! </h1>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-col md:flex-row items-center">
 
                 <div className='w-1/2 pl-6'>
                     <AboutMeAccordion></AboutMeAccordion>
