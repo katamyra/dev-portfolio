bfe552eae70296852533c60ca57b5c1c2f96d51c
Krish Katariya <krish.katariya@hotmail.com>
2024-03-21 21:36:41 -0700

    Correctly calls repos now

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,7 +3,12 @@ import { NextResponse } from "next/server";
 // To handle a GET request to /api
 export async function GET(request) {
   // Do whatever you want
-  return NextResponse.json({ message: "Hello World GET" }, { status: 200 });
+  const response = await fetch(`https://api.github.com/users/katamyra/repos`);
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+  const data = await response.json();
+  return NextResponse.json(data, { status: 200 });
 }
 
 // To handle a POST request to /api
--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -4,24 +4,25 @@ import AnimatedCursor from "react-animated-cursor";
 import { useEffect, useState } from "react";
 export default function Testing() {
   // Example of fetching data from the client-side component
-useEffect(() => {
-  async function fetchRepos() {
-    const response = await fetch('/api');
-    if (!response.ok) {
-      console.error('Failed to fetch repos');
-      return;
+  useEffect(() => {
+    async function fetchRepos() {
+      const response = await fetch('/api');
+      if (!response.ok) {
+        console.error('Failed to fetch repos');
+        return;
+      }
+
+      const repos = await response.json();
+      console.log("a");
+      console.log(repos); // Do something with the repository data
     }
 
-    const repos = await response.json();
-    console.log(repos); // Do something with the repository data
-  }
+    fetchRepos();
+  }, []);
 
-  fetchRepos();
-}, []);
-
-  return (
-    <div>
-      <AnimatedCursor trailingSpeed={5} outerSize={5}></AnimatedCursor>
-    </div>
-  );
+    return (
+      <div>
+        <AnimatedCursor trailingSpeed={5} outerSize={5}></AnimatedCursor>
+      </div>
+    );
 }
